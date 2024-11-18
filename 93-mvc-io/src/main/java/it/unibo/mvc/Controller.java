06adfbfb457bd1627314860d6541be93f0a67e47
Matteo Caruso <matteo.caruso7@studio.unibo.it>
2024-11-18 13:42:56 +0100

    Started 93 by creating the Controller interface

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,30 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ * Controller interface.
  */
 public interface Controller {
+    /**
+     * Sets the current string.
+     * 
+     * @param string the next string to print.
+     */
+    void setString(String string);
 
+    /**
+     * @return the next string to print.
+     */
+    String getString();
+
+    /**
+     * @return a list that contains every string that was printed.
+     */
+    List<String> getPrintedHistory();
+
+    /**
+     * Prints the current string, and adds it to the history.
+     */
+    void printString();
 }
