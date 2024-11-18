611285c1c03471bd6bc60e977ddb0d7bda1ab180
Matteo Caruso <matteo.caruso7@studio.unibo.it>
2024-11-18 13:43:12 +0100

    Continued 93 by implementing SimpleController

--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,41 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
- * 
- *
+ * Implementation of Controller.
  */
 public final class SimpleController implements Controller {
+
+    // Last string is the current string.
+    private final List<String> strings = new ArrayList<>();
+    private String currenString;
+
+    @Override
+    public void setString(final String string) {
+        Objects.requireNonNull(string);
+        this.currenString = string;
+    }
+
+    @Override
+    public String getString() {
+        if (currenString == null) {
+            throw new IllegalStateException("No string was set.");
+        }
+        return this.currenString;
+    }
+
+    @Override
+    public List<String> getPrintedHistory() {
+        return List.copyOf(this.strings);
+    }
+
+    @Override
+    public void printString() {
+        System.out.println(getString()); // NOPMD: printing is required by the excercise.
+        this.strings.addLast(currenString);
+    }
 
 }
